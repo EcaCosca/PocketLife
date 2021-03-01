6eb11a77d919843b69cd292ef0bd61ae99e52f9d
EcaCosca <enriquecoscarelli@gmail.com>
2021-03-01 14:52:47 +0100

    First Commit

---
+++ Pokemon Unity/Assets/Splash/SplashSequence.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashSequence : MonoBehaviour {
+
+    public Animator animator;
+
+
+    public static int SceneNumber;
+
+	// Use this for initialization
+	void Start () {
+		if(SceneNumber == 0)
+        {
+            StartCoroutine(ToSplashTwo());
+        }
+     
+    }
+	
+    IEnumerator ToSplashTwo ()
+    {
+        yield return new WaitForSeconds(3);
+
+
+        SceneNumber = 1;
+       SceneManager.LoadScene(1);
+
+    }
+}
