a1dfcb1aec170d57f545bcc281dbe061d116b04b
Joseph Buckley <jbuckley213@gmail.com>
2021-03-02 10:58:21 +0100

    Two Bridges Fixed

---
+++ Pokemon Unity/Assets/SceneTranstitionNew.cs
@@ -0,0 +1,40 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class SceneTranstitionNew : MonoBehaviour {
+
+
+
+
+
+    public Animator animator;
+
+
+    public static int SceneNumber;
+
+    // Use this for initialization
+   
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Scene Transistion");
+
+        StartCoroutine(ToSplashTwo());
+    }
+
+    IEnumerator ToSplashTwo()
+    {
+        yield return new WaitForSeconds(3);
+
+
+        SceneNumber = 1;
+        SceneManager.LoadScene(1);
+
+    }
+}
+
+
